bab319bc0f5be8d0b6e511787db9c7a1b62ff0b6
Xavier Diaz <xdiaz@ucsc.edu>
2020-06-04 18:06:22 -0700

    show addresses within 25 miles of given address on home

--- Projects/DeliveryApp/ClientApp/src/app/models/address.ts
+++ Projects/DeliveryApp/ClientApp/src/app/models/address.ts
@@ -1,10 +1,12 @@
 export class Address {
+    id: number;
     address1: string;
     address2: string;
     city: string;
     state: string;
     zipCode: string;
-    constructor(address1: string, address2: string, city: string, state: string, zipCode: string){
+    constructor(id: number, address1: string, address2: string, city: string, state: string, zipCode: string){
+        this.id = id;
         this.address1 = address1;
         this.address2 = address2;
         this.city = city;
