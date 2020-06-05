476b6e2bc4311e9a6f611cf104ed611470989aa9
roxfordlc <roxfordlc@gmail.com>
2020-06-04 21:00:43 -0500

    Address validation added

--- Projects/DeliveryApp/DeliveryApp.Data/Objects/Address.cs
+++ Projects/DeliveryApp/DeliveryApp.Data/Objects/Address.cs
@@ -11,19 +11,22 @@ namespace DeliveryApp.Data.Objects
         [Key]
         public int ID { get; set; }
         [Required]
+        [RegularExpression(InputValidation.address1Pattern, ErrorMessage = InputValidation.addressError)]
         [DisplayName("Address Line 1")]
         public string Address1 { get; set; }
         [Required]
         [DisplayName("Address Line 2")]
+        [RegularExpression(InputValidation.address2Pattern, ErrorMessage = InputValidation.addressError)]
         public string Address2 { get; set; }
         [Required]
+        [RegularExpression(InputValidation.cityPattern, ErrorMessage = InputValidation.cityError)]
         [DisplayName("City")]
         public string City { get; set; }
-        [Required]
+        [UsStateTwoCharacterAbbreviation]
         [DisplayName("State")]
         public string State { get; set; }
-        [Required]
         [DisplayName("Zip Code")]
+        [RegularExpression(InputValidation.zipPattern, ErrorMessage = InputValidation.zipError)]
         public string ZipCode { get; set; }
         //public int UserID { get; set; } // This is a navigation property, allows us to acces the User Property .. below
         //public User User { get; set; }
