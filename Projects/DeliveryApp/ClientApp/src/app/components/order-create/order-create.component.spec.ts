78437abbcdde2bfba8d852854677260f8b6c442b
Darius <darius.vallejo@gmail.com>
2020-06-04 20:51:55 -0700

    Merge/testing

--- Projects/DeliveryApp/ClientApp/src/app/components/order-create/order-create.component.spec.ts
+++ Projects/DeliveryApp/ClientApp/src/app/components/order-create/order-create.component.spec.ts
@@ -24,7 +24,7 @@ describe('OrderCreateComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
